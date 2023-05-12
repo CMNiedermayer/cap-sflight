76823ef73b3f7cb5d396a7b40080480f7e231707
CMNiedermayer <55582667+CMNiedermayer@users.noreply.github.com>
2023-05-12 10:53:12 +0200

    Update Component.js

--- app/travel_analytics/webapp/Component.js
+++ app/travel_analytics/webapp/Component.js
@@ -1,6 +1,10 @@
-sap.ui.define(["sap/fe/core/AppComponent"], function (Component) {
+sap.ui.define(["sap/fe/core/AppComponent", "sap/ui/performance/trace/FESR"], function (Component, FESR) {
   "use strict";
   return Component.extend("sap.fe.cap.travel_analytics.Component", {
     metadata: { manifest: "json" },
+    init: function() {
+    	FESR.setActive(true, this.getModel().getServiceUrl().substr(2) + "../fesr");
+    	Component.prototype.init.apply(this, arguments);
+    }
   });
 });
